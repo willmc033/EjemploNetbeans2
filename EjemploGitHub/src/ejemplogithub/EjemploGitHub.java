169d3f95b354c236d4895656b99c46b4995e61fd
javam2018 <javam2018@S10-96642.redes.local>
2018-07-12 11:06:00 -0600

    se agregaron dos nuevos mensajes

--- EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
+++ EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
@@ -4,6 +4,8 @@ public class EjemploGitHub {
 
     public static void main(String[] args) {
         System.out.println("Hola ke ace");
+        System.out.println("ok no");
+        System.out.println("Prueva 1");
     }
     
 }
